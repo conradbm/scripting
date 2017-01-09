2236617926a7957f453e1357d0f623c63ec8f5aa
Blake Conrad <bmconrad@iupui.edu>
2017-01-08 22:11:05 -0500

    initial commit

---
+++ copyAllFolders.sh
@@ -0,0 +1,11 @@
+# Written by: Blake Conrad
+# Content: Iterative Numeric Loop to copy 16 Folders with all their 
+# contents
+# Resource: https://www.cyberciti.biz/faq/bash-for-loop/
+
+# recursively copy every file in any folder in the regular expression 
+# like week_* up one folder into the folder specified..
+cp -r week_* ../CSCI-49000\ \(Data_Science\)/
+cp -r week_* ../CSCI-43200\ \(Security_in_Computing\)/
+cp -r week_* ../GEOL-110\ \(Physical_Geology\)/
+cp -r week_* ../CSCI-N341\ \(Client_Side_Web_Programming\)/
